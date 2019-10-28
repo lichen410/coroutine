3f17a5a347f35308dd0f7a58964e77b75bb7a8b1
lichen410 <46471417+lichen410@users.noreply.github.com>
2019-10-28 21:21:43 +0800

    Update coroutine.h

--- coroutine.h
+++ coroutine.h
@@ -7,6 +7,7 @@
 #include<iostream>
 #include<vector>
 #include<map>
+#include<list>
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -15,6 +16,7 @@
 #include <netinet/in.h>
 #include <sys/epoll.h>
 #include <sys/resource.h>
+using namespace std;
 struct routine{
     //int id;//这个协程的标识
     //int f;//是否结束的标识
@@ -23,13 +25,37 @@ struct routine{
 struct timelistnode{
     routine *t;
     time_t sec;
-    timelistnode* next;
-    timelistnode *pre;
 };
-struct timelist{
-    timelistnode *head;
-    timelistnode *tail;
-    int n=0;
+
+
+class timelist{
+   list<timelistnode*> time_list;
+
+public:
+    int min_time;
+    timelist(){
+        min_time=3;
+    }
+    void add(timelistnode* p_time_node){
+        if(p_time_node->sec-time(0)<min_time) min_time=p_time_node->sec-time(0);
+        time_list.push_back(p_time_node);
+    }
+    vector<timelistnode*> loop_find_end_time(){
+        vector<timelistnode*> ans;
+        min_time=3;
+        list<timelistnode*>::iterator iter=time_list.begin();
+        for(;iter!=time_list.end();){
+            if((*iter)->sec<=time(0)){
+                ans.push_back(*iter);
+                time_list.erase(iter++);
+            }
+            else{
+                if((*iter)->sec-time(0)<min_time) min_time=(*iter)->sec-time(0);
+                iter++;
+            }
+        }
+        return ans;
+    }
 };
  routine* makeroutine(void (*)(routine *));
  void start_routine(routine * );
